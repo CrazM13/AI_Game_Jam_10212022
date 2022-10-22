b306b48fc6acdec9d5eff294e09ba585504be8d4
Rui Santos <cardkingrui@gmail.com>
2022-10-22 14:12:55 -0400

    Camera Moves with Player
    Camera Moves with Player

--- PR_AIGameJam_3D/Assets/_Scripts/PlayerController/PlayerMovement.cs
+++ PR_AIGameJam_3D/Assets/_Scripts/PlayerController/PlayerMovement.cs
@@ -9,22 +9,28 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 pos = this.transform.position;
+        Vector3 cameraPos = Camera.main.transform.position;
         if (Input.GetKey("w"))
         {
             pos.z += speed * Time.deltaTime;
+            cameraPos.z += speed * Time.deltaTime;
         }
         if (Input.GetKey("s"))
         {
             pos.z -= speed * Time.deltaTime;
+            cameraPos.z -= speed * Time.deltaTime;
         }
         if (Input.GetKey("d"))
         {
             pos.x += speed * Time.deltaTime;
+            cameraPos.x += speed * Time.deltaTime;
         }
         if (Input.GetKey("a"))
         {
             pos.x -= speed * Time.deltaTime;
+            cameraPos.x -= speed * Time.deltaTime;
         }
         this.transform.position = pos;
+        Camera.main.transform.position = cameraPos;
     }
 }
