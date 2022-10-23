51dd94ca5fa8abe64cb9cc0132849ef1e0274bcc
Rui Santos <cardkingrui@gmail.com>
2022-10-23 15:43:44 -0400

    Player Rotation
    Player Rotation finished.

--- PR_AIGameJam_3D/Assets/_Scripts/PlayerController/PlayerMovement.cs
+++ PR_AIGameJam_3D/Assets/_Scripts/PlayerController/PlayerMovement.cs
@@ -5,39 +5,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]private float speed = 8.5f;
-    [SerializeField] private float rotationSpeed = 205f;
+    [SerializeField] private float rotationSpeed = 250f;
 
     private void Update()
     {
-        Vector3 pos = this.transform.position;
+        Vector3 pos = transform.position;
         Vector3 cameraPos = Camera.main.transform.position;
-        Quaternion toRotate = Quaternion.LookRotation(pos, Vector3.up);
 
         if (Input.GetKey("w"))
         {
             pos.z += speed * Time.deltaTime;
             cameraPos.z += speed * Time.deltaTime;
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, toRotate, rotationSpeed * 10 * Time.deltaTime);
+            Vector3 target = pos - transform.position;
+            Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey("s"))
         {
             pos.z -= speed * Time.deltaTime;
             cameraPos.z -= speed * Time.deltaTime;
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, toRotate, rotationSpeed * 10 * Time.deltaTime);
+            Vector3 target = pos - transform.position;
+            Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey("d"))
         {
             pos.x += speed * Time.deltaTime;
             cameraPos.x += speed * Time.deltaTime;
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, toRotate, rotationSpeed * 10 * Time.deltaTime);
+            Vector3 target = pos - transform.position;
+            Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey("a"))
         {
             pos.x -= speed * Time.deltaTime;
             cameraPos.x -= speed * Time.deltaTime;
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, toRotate, rotationSpeed *10* Time.deltaTime);
+            Vector3 target = pos - transform.position;
+            Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
         }
-        this.transform.position = pos;
+        transform.position = pos;
         Camera.main.transform.position = cameraPos;
     }
 }
