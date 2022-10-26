18dd0319badeecf34ea2382cfb975b00e2fc1fb3
Rui Santos <cardkingrui@gmail.com>
2022-10-25 22:05:39 -0400

    Added Cross and Fixed Diagonal
    Diagonal Speed is now halved to reduce its overall diagonal speed. Cross Tag has been added to give us easy boundaries without collision boxes.

--- PR_AIGameJam_3D/Assets/_Scripts/PlayerController/PlayerMovement.cs
+++ PR_AIGameJam_3D/Assets/_Scripts/PlayerController/PlayerMovement.cs
@@ -6,29 +6,123 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]private float speed = 8.5f;
     [SerializeField] private float rotationSpeed = 250f;
+    [SerializeField] GameObject[] Crosses;
+    [SerializeField] private Transform MaxX;
+    [SerializeField] private Transform MinX;
+    [SerializeField] private Transform MaxZ;
+    [SerializeField] private Transform MinZ;
+    private Vector3 pos;
+    private Vector3 cameraPos;
+    private Vector3 startingCamPos;
+
+    private void Start()
+    {
+        startingCamPos = Camera.main.transform.position;
+        Crosses = GameObject.FindGameObjectsWithTag("Cross");
+        for(int i=0; i < Crosses.Length; i++)
+        {
+            if(Crosses[i].transform.position.x > 0)
+            {
+                MaxX = Crosses[i].transform;
+            }
+            else if (Crosses[i].transform.position.x < 0)
+            {
+                MinX = Crosses[i].transform;
+            }
+            else if (Crosses[i].transform.position.z > 0)
+            {
+                MaxZ = Crosses[i].transform;
+            }
+            else if (Crosses[i].transform.position.z < 0)
+            {
+                MinZ = Crosses[i].transform;
+            }
+        }
+    }
 
     private void Update()
     {
         Vector3 pos = transform.position;
         Vector3 cameraPos = Camera.main.transform.position;
 
-        if (Input.GetKey("w"))
+        if (pos.z > MaxZ.position.z)
         {
-            pos.z += speed * Time.deltaTime;
-            cameraPos.z += speed * Time.deltaTime;
-            Vector3 target = pos - transform.position;
-            Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            pos.z = MaxZ.position.z;
+            cameraPos.z = MaxZ.position.z + startingCamPos.z;
         }
-        if (Input.GetKey("s"))
+        else if (Input.GetKey("w")&&(pos.z < MaxZ.position.z))
         {
-            pos.z -= speed * Time.deltaTime;
-            cameraPos.z -= speed * Time.deltaTime;
-            Vector3 target = pos - transform.position;
-            Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            if (Input.GetKey("d"))
+            {
+                pos.z += speed / 2 * Time.deltaTime;
+                pos.x += speed / 2 * Time.deltaTime;
+                cameraPos.x += speed / 2 * Time.deltaTime;
+                cameraPos.z += speed / 2 * Time.deltaTime;
+                Vector3 target = pos - transform.position;
+                Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey("a"))
+            {
+                pos.z += speed / 2 * Time.deltaTime;
+                pos.x -= speed / 2 * Time.deltaTime;
+                cameraPos.x -= speed / 2 * Time.deltaTime;
+                cameraPos.z += speed / 2 * Time.deltaTime;
+                Vector3 target = pos - transform.position;
+                Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                pos.z += speed * Time.deltaTime;
+                cameraPos.z += speed * Time.deltaTime;
+                Vector3 target = pos - transform.position;
+                Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            }
         }
-        if (Input.GetKey("d"))
+        if(pos.z < MinZ.position.z)
+        {
+            pos.z = MinZ.position.z;
+            cameraPos.z = MinZ.position.z + startingCamPos.z;
+        }
+        else if (Input.GetKey("s")&&(pos.z>MinZ.position.z))
+        {
+            if (Input.GetKey("d"))
+            {
+                pos.z -= speed / 2 * Time.deltaTime;
+                pos.x += speed / 2 * Time.deltaTime;
+                cameraPos.x += speed / 2 * Time.deltaTime;
+                cameraPos.z -= speed / 2 * Time.deltaTime;
+                Vector3 target = pos - transform.position;
+                Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey("a"))
+            {
+                pos.z -= speed / 2 * Time.deltaTime;
+                pos.x -= speed / 2 * Time.deltaTime;
+                cameraPos.x -= speed / 2 * Time.deltaTime;
+                cameraPos.z -= speed/2 * Time.deltaTime;
+                Vector3 target = pos - transform.position;
+                Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                pos.z -= speed * Time.deltaTime;
+                cameraPos.z -= speed * Time.deltaTime;
+                Vector3 target = pos - transform.position;
+                Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            }
+        }
+        if(pos.x > MaxX.position.x)
+        {
+            pos.x = MaxX.position.x;
+            cameraPos.x = MaxX.position.x + startingCamPos.x;
+        }
+        else if (Input.GetKey("d")&&(pos.x < MaxX.position.x))
         {
             pos.x += speed * Time.deltaTime;
             cameraPos.x += speed * Time.deltaTime;
@@ -36,7 +130,12 @@ public class PlayerMovement : MonoBehaviour
             Quaternion toRotate = Quaternion.LookRotation(target, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
         }
-        if (Input.GetKey("a"))
+        if(pos.x < MinX.position.x)
+        {
+            pos.x = MinX.position.x;
+            cameraPos.x = MinX.position.x + startingCamPos.x;
+        }
+        else if (Input.GetKey("a")&&(pos.x>MinX.position.x))
         {
             pos.x -= speed * Time.deltaTime;
             cameraPos.x -= speed * Time.deltaTime;
